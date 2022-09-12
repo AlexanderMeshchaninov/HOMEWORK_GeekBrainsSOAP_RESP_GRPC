3b6df4a875b6adb408b0ae7466f3a81fa7b05e9c
Alexander <alexander_freeman85@mail.ru>
2022-09-13 00:32:03 +0300

    Lesson-2 added

---
+++ Lesson-2/PumpService/PumpClient/CallbackHandler.cs
@@ -0,0 +1,17 @@
+﻿using PumpClient.PumpServiceReference;
+using System;
+
+namespace PumpClient
+{
+    public class CallbackHandler : IPumpServiceCallback
+    {
+        public void UpdateStatistics(StatisticsService statistics)
+        {
+            Console.Clear();
+            Console.WriteLine("Обновление по статистике выполнения скрипта:");
+            Console.WriteLine($"Всего     запусков: {statistics.AllCounts}");
+            Console.WriteLine($"Успешных  запусков: {statistics.SuccessCounts}");
+            Console.WriteLine($"Ошибочных запусков: {statistics.ErrorCounts}");
+        }
+    }
+}
---
+++ Lesson-2/PumpService/PumpService/Core/IPumpServiceCallback.cs
@@ -0,0 +1,12 @@
+﻿using PumpService.Services;
+using System.ServiceModel;
+
+namespace PumpService.Core
+{
+    [ServiceContract]
+    public interface IPumpServiceCallback
+    {
+        [OperationContract]
+        void UpdateStatistics(StatisticsService statistics);
+    }
+}
